a2efd4c58f443d8bb125d84fc34ddd3c47a89b9d
= <=>
2025-06-19 00:40:20 +0500

    copleted add store page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,28 @@
-import Header from "../components/Header.tsx"
-import Topbar from "../components/TopBar.tsx"
-import Cards from "../components/Cards.tsx"
+import Topbar from "../components/TopBar.tsx";
+import Cards from "../components/Cards.tsx";
+import { useRequireStore } from "../hooks/useRequireStore";
 
 function Home() {
+  const currentStore = useRequireStore();
+
+  // Show loading state while checking for store
+  if (!currentStore) {
+    return (
+      <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+        <div className="text-center">
+          <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-teal-600 mx-auto mb-4"></div>
+          <p className="text-gray-600">Loading store information...</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
-        
-        <Topbar />
-        <Cards />
+      <Topbar />
+      <Cards />
     </div>
-  )
+  );
 }
 
-
-export default Home
+export default Home;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,10 +4,11 @@ import Login from '../pages/Login/Login';
 import OtpPage from '../pages/OtpPage/OtpPage';
 import Inventory from '../pages/Inventory/Inventory';
 import Home from '../pages/Home';
-import Terms from '../pages/Terms'
-import StoreForm from '../components/StoreForm'
-import ExpensePage from '../pages/expensePage/Expense'
-import SalesPage from '../pages/salesPage/Sales'
+import Terms from '../pages/Terms';
+import StoreForm from '../components/StoreForm';
+import StoresPage from '../pages/Stores/Stores';
+import ExpensePage from '../pages/expensePage/Expense';
+import SalesPage from '../pages/salesPage/Sales';
 
 const AppRoutes = () => {
   return (
@@ -15,6 +16,9 @@ const AppRoutes = () => {
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/otp" element={<OtpPage />} />
+      <Route path="/stores" element={<StoresPage />} />
+      <Route path="/store/create" element={<StoreForm />} />
+      <Route path="/store/edit/:id" element={<StoreForm />} />
       <Route path="/inventory" element={<Inventory />} />
       <Route path="/" element={<Home />} />
       <Route path="/term" element={<Terms />} />
