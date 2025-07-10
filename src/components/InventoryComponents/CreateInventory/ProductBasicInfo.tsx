fea30899106516ea844e140f73a325b08d23d542
hassanjan-innova360 <hassanjan@innova360.io>
2025-07-11 01:02:15 +0500

    refactor: update supplier selection message in ProductBasicInfo component to clarify that it is optional

--- src/components/InventoryComponents/CreateInventory/ProductBasicInfo.tsx
+++ src/components/InventoryComponents/CreateInventory/ProductBasicInfo.tsx
@@ -611,30 +611,10 @@ const ProductBasicInfo: React.FC<ProductBasicInfoProps> = ({
                   {suppliersError}
                 </div>
               )}
-              {/* Show helpful message when no suppliers exist */}
-              {!suppliersLoading &&
-                !suppliersError &&
-                (!suppliers || suppliers.length === 0) && (
-                  <div className="mt-2 p-3 bg-yellow-50 border border-yellow-200 rounded-lg">
-                    <div className="flex items-center space-x-2 text-yellow-800 text-sm">
-                      <svg
-                        className="w-4 h-4"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                      <span>
-                        No suppliers found. Please add suppliers first to
-                        continue.
-                      </span>
-                    </div>
-                  </div>
-                )}
+              {/* Note: Supplier selection is optional */}
+              <div className="mt-1 text-xs text-gray-500 italic">
+                Selecting a supplier is optional. You can add or assign a supplier later.
+              </div>
             </div>
 
             <div className="space-y-2">
